9ab7bedf2e7c2261dc8e18b6f7f7cdc1424246b7
Faizan <faizanburney@live.com>
2020-05-11 05:23:49 +0500

    in case of internal sorting is not desired.

--- src/main/java/EvenOddComparator.java
+++ src/main/java/EvenOddComparator.java
@@ -5,7 +5,7 @@ public class EvenOddComparator implements Comparator<Integer> {
     public int compare(Integer o1, Integer o2) {
         //same either odd or even
         if(o1%2 == o2%2)
-        {
+        {// just return 0 in case internal sorting is not desired.
             if(o1<o2) //sort with in same even/odd
                 return -1;
             else if(o2>01)
